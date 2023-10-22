cce11486e384beaf33364acfff131e485787368b
Indresh Yadav <147027546+IndreshYadav003@users.noreply.github.com>
2023-10-22 16:27:05 +0530

    Sort Linked ListNon Decending order

---
+++ sortlinkedlist2.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node 
+{
+    int num;                        //Data of the node
+    struct node *nextptr;           //Address of the next node
+}*stnode;
+
+//creat a nodes;
+struct node *createNodeList(int n)
+{
+    struct node *fnNode, *tmp;
+    int num, i;
+    stnode = (struct node *)malloc(sizeof(struct node));
+
+    if(stnode == NULL) //check whether the fnnode is NULL and if so no memory allocation
+    {
+        printf(" Memory can not be allocated.");
+        return stnode;
+    }
+    else
+    {
+// reads data for the node through keyboard
+
+        printf(" Input data for node 1 : ");
+        scanf("%d", &num);
+        stnode->num = num;      
+        stnode->nextptr = NULL; // links the address field to NULL
+        tmp = stnode;
+// Creating n nodes and adding to linked list
+        for(i=2; i<=n; i++)
+        {
+            fnNode = (struct node *)malloc(sizeof(struct node));
+            if(fnNode == NULL)
+            {
+                printf(" Memory can not be allocated.");
+                break;
+            }
+            else
+            {
+                printf(" Input data for node %d : ", i);
+                scanf(" %d", &num);
+ 
+                fnNode->num = num;      // links the num field of fnNode with num
+                fnNode->nextptr = NULL; // links the address field of fnNode with NULL
+ 
+                tmp->nextptr = fnNode; // links previous node i.e. tmp to the fnNode
+                tmp = tmp->nextptr; 
+            }
+        }
+    }
+    return stnode;
+}
+void displayList(struct node *start)
+{
+    
+    if(start== NULL)
+    {
+        printf(" List is empty.");
+    }
+    else
+    {
+      struct node  *tmp = start;
+        while(tmp != NULL)
+        {
+            printf(" Data = %d\t", tmp->num);       // prints the data of current node
+            tmp = tmp->nextptr;                     // advances the position of current node
+        }
+    }
+} 
+struct node *sortlinkedlist(struct node *start){
+    struct node  *ptr,*preptr;
+    int val;
+    ptr=start;
+    while (ptr->nextptr!=NULL)
+    {
+        preptr=ptr->nextptr;
+        while ( preptr!= NULL)
+        {
+            if(ptr->num>preptr->num){
+                val=ptr->num;
+                ptr->num=preptr->num;
+                preptr->num=val;
+            }
+            preptr=preptr->nextptr;
+        }
+        ptr=ptr->nextptr;
+        
+    }
+    
+    return start;
+}
+int main()
+{
+    int n;
+		
+    printf(" Input the number of nodes : ");
+    scanf("%d", &n);
+   stnode= createNodeList(n);
+    printf("\n Data entered in the list : \n");
+    displayList(stnode);
+    printf("\nAfter Sorting Linked List\n");
+    struct node *ptr=sortlinkedlist(stnode);
+     displayList(ptr);
+    
+    return 0;
+}
+ //Data entered in the list :
+ //Data = 43       Data = 32       Data = 4        Data = 212      Data = 6  
+//After Sorting Linked List
+// Data = 4        Data = 6        Data = 32       Data = 43       Data = 212
